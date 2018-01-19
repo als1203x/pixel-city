d94fc75a6052f3bc3dc13eb2cc74155ec321ecb1
LinuxPlus <linuxplus@linuxpluss-mac.local>
2018-01-19 06:28:10 -0800

    Set locationServices; Set p.list rules for user privacy permissions; center location functionality complete

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -8,24 +8,54 @@
 
 import UIKit
 import MapKit
+import CoreLocation
+
 
 class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
+    let authorizationStatus = CLLocationManager.authorizationStatus() // returns Int32
+    let regionRadius: Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         mapView.delegate = self
+        configureLocationServices()
     }
 
     
-    
     @IBAction func locationBtnPressed(_ sender: UIButton) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse  {
+            centerMapOnUserLocation()
+        }
     }
-    
 }
 
 extension MapVC: MKMapViewDelegate  {
+        // Zoom on user location
+    func centerMapOnUserLocation()  {
+        guard let coordinate = locationManager.location?.coordinate else    { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate  {
+    
+    func configureLocationServices()    {
+        if authorizationStatus == .notDetermined    {
+            locationManager.requestAlwaysAuthorization()
+        }else   {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
     
 }
 
